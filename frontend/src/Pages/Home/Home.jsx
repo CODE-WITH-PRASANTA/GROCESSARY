1c5851de00e8e9352c3e8c136987b4aff21ab4d3
Prasanta Kumar Khuntia <prasantakhuntia2018@gmail.com>
2026-08-09 17:41:51 +0530

    Done

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -23,7 +23,7 @@ const Home = () => {
       {/* <OurBestsellers /> */}
       {/* <NourishSection /> */}
       {/* <FreashItem /> */}
-      <Testimonial />
+      {/* <Testimonial /> */}
       {/* <Blog /> */}
     </div>
   )
