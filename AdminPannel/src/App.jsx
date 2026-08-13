1363ade5af9236aa16521ba52d68852aafafbee6
adyasa974 <adbarik60@gmail.com>
2026-08-13 11:49:15 +0530

    done

--- AdminPannel/src/App.jsx
+++ AdminPannel/src/App.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+// Layout
 import MainLayout from "./Layout/MainLayout/MainLayout";
-import ProtectedRoute from "./Components/ProtectedRoute/ProtectedRoute";
-import Login from "./Components/Login/Login";
-import Dashboard from "./Pages/Dashboard/Dashboard";
-import Blog from "./Components/Blog/Blog";
-import BlogManagement from "./Components/BlogManagement/BlogManagement";
+
+// Component / Page Imports
 import TestimonialManagement from "./Components/TestimonialManagement/TestimonialManagement";
 import Allproduct from "./Components/Blog/AllProduct/Allproduct";
-
+import Addnew from "./Components/Blog/AddProducts/AddProducts";
 import ReturnManagement from "./Components/Blog/ReturnManagement/ReturnManagement";
 import ReviewsManagement from "./Components/Blog/ReviewsManagement/ReviewsManagement";
 import ReturnDetails from "./Components/Blog/ReturnDetails/ReturnDetails";
@@ -20,67 +19,79 @@ import ReturnApproval from "./Components/Blog/ReturnApproval/ReturnApproval";
 import PickupManagement from "./Components/Blog/PickupManagement/PickupManagement";
 import InventoryAdjustment from "./Components/Blog/InventoryAdjustment/InventoryAdjustment";
 import ActivityLog from "./Components/Blog/ActivityLog/ActivityLog";
-import Order from "./Pages/Order/Order";
-import Categories from "./Components/Categories/Categories";
-import Brands from "./Components/Brands/Brands";
-import Discounts from "./Components/Discounts/Discounts";
 import EditUnit from "./Components/EditUnit/EditUnit";
 import ListUploads from "./Components/ListUploads/ListUploads";
 import Banners from "./Components/Banners/Banners";
+import BlogPosting from "./Components/BlogPosting/BlogPosting";
+import BlogManagement from "./Components/BlogManagement/BlogManagement";
+
+// Previously Missing Imports (Update file paths according to your folder structure)
+
+
+import Categories from "./Components/Categories/Categories";
+import Brands from "./Components/Brands/Brands";
+import Discounts from "./Components/Discounts/Discounts";
+
+import Dashboard from "./Pages/Dashboard/Dashboard";
+import Order from "./Pages/Order/Order";
 import ColdLeadManagement from "./Components/ColdLeadManagement/ColdLeadManagement";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        {/* Public Route */}
-        <Route path="/login" element={<Login />} />
+        {/* Main Layout Wrap */}
+        <Route path="/" element={<MainLayout />}>
+          
+          {/* Default Redirect */}
+          <Route index element={<Navigate to="/dashboard" replace />} />
 
-        {/* Protected Routes (Wrapped with ProtectedRoute & MainLayout) */}
-        <Route element={<ProtectedRoute />}>
-          <Route path="/" element={<MainLayout />}>
-            {/* Redirect root path to dashboard */}
-            <Route index element={<Navigate to="/dashboard" replace />} />
-            
-            {/* Dashboard & Core */}
-            <Route path="dashboard" element={<Dashboard />} />
-            <Route path="coldlead" element={<ColdLeadManagement />} />
+          {/* Dashboard */}
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="/coldlead" element={<ColdLeadManagement />} />
 
-            {/* Blog & Testimonials */}
-            <Route path="blog" element={<Blog />} />
-            <Route path="blogmanagement" element={<BlogManagement />} />
-            <Route path="testimonialmanagement" element={<TestimonialManagement />} />
+          {/* Blog & Testimonials */}
+          <Route path="blog" element={<BlogPosting />} />
+          <Route path="blogmanagement" element={<BlogManagement />} />
+          <Route path="testimonialmanagement" element={<TestimonialManagement />} />
 
-            {/* Products */}
-            <Route path="products/all-products" element={<Allproduct />} />
-           
-            <Route path="products/categories" element={<Categories />} />
-            <Route path="products/brands" element={<Brands />} />
-            <Route path="products/units" element={<EditUnit />} />
-            <Route path="products/list-uploads" element={<ListUploads />} />
+          {/* Products */}
+          <Route path="products/all-products" element={<Allproduct />} />
+          <Route path="products/add-product" element={<Addnew />} />
+          <Route path="products/categories" element={<Categories />} />
+          
+          <Route path="products/brands" element={<Brands />} />
+          <Route path="products/units" element={<EditUnit />} />
+          <Route path="products/list-uploads" element={<ListUploads />} />
 
-            {/* Orders & Returns */}
-            <Route path="orders/all-orders" element={<Order />} />
-            <Route path="orders/returns" element={<ReturnManagement />} />
-            <Route path="returns/details" element={<ReturnDetails />} />
-            <Route path="returns/inspection" element={<QualityInspection />} />
-            <Route path="returns/refund" element={<RefundDetails />} />
-            <Route path="returns/product-info" element={<ProductInformation />} />
-            <Route path="returns/replacement-details" element={<ReplacementDetails />} />
-            <Route path="returns/approval" element={<ReturnApproval />} />
-            <Route path="returns/pickup-management" element={<PickupManagement />} />
-            <Route path="returns/inventory-adjustment" element={<InventoryAdjustment />} />
-            <Route path="returns/activity-log" element={<ActivityLog />} />
+          {/* Orders */}
+          <Route path="orders/all-orders" element={<Order />} />
+          <Route path="orders/returns" element={<ReturnManagement />} />
 
-            {/* Customers & Marketing */}
-            <Route path="customers/reviews" element={<ReviewsManagement />} />
-            <Route path="marketing/banners" element={<Banners />} />
-            <Route path="marketing/discounts" element={<Discounts />} />
-          </Route>
+          {/* Customers */}
+          
+          <Route path="customers/reviews" element={<ReviewsManagement />} />
+
+          {/* Marketing */}
+          <Route path="marketing/banners" element={<Banners />} />
+          <Route path="marketing/discounts" element={<Discounts />} />
+        
+
+          {/* Returns */}
+          <Route path="returns/details" element={<ReturnDetails />} />
+          <Route path="returns/inspection" element={<QualityInspection />} />
+          <Route path="returns/refund" element={<RefundDetails />} />
+          <Route path="returns/product-info" element={<ProductInformation />} />
+          <Route path="returns/replacement-details" element={<ReplacementDetails />} />
+          <Route path="returns/approval" element={<ReturnApproval />} />
+          <Route path="returns/pickup-management" element={<PickupManagement />} />
+          <Route path="returns/inventory-adjustment" element={<InventoryAdjustment />} />
+          <Route path="returns/activity-log" element={<ActivityLog />} />
+
+          {/* Settings */}
+         
+
         </Route>
-
-        {/* Catch-all redirect to login */}
-        <Route path="*" element={<Navigate to="/login" replace />} />
       </Routes>
     </BrowserRouter>
   );
