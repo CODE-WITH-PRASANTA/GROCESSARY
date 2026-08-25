68261f4013835076b2b5308edee2fab7cb8a6a4a
Prasanta Kumar Khuntia <prasantakhuntia2018@gmail.com>
2026-08-25 12:34:37 +0530

    Done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -51,18 +51,7 @@ const App = () => {
           path="/BlogReadmoreOne"
           element={<BlogReadmoreOne />}
         />
-        {/* <Route
-          path="/BlogReadmoreTwo"
-          element={<BlogReadmoreTwo />}
-        />
-        <Route
-          path="/BlogReadmoreThree"
-          element={<BlogReadmoreThree />}
-        />
-        <Route
-          path="/BlogReadmoreFour"
-          element={<BlogReadmoreFour />}
-        /> */}
+       
         <Route path="/account" element={<LoginPage />} />
         <Route path="/cart" element={<CartSection />} />
         <Route path="/deliverytime" element={<DeliveryTime />} />
